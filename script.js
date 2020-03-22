dead3f8aed03f6afd59d4d4bf655ecccb5e495f1
Uzun Igor <te10@mail.ru>
2020-03-22 10:52:54 +0200

    made modification on formating

--- script.js
+++ script.js
@@ -4,13 +4,5 @@ divElement.addEventListener('click', function(event) {
     let spanMessage = document.getElementById('span');
     let button = event.target.closest('button');
     if (button == null) return;
-    if (event.target.closest('button.container__first')) {
-        spanMessage.innerText = "You clicked First button";
-    }
-    if (event.target.closest('button.container__second')) {
-        spanMessage.innerText = "You clicked Second button";
-    }
-    if (event.target.closest('button.container__third')) {
-        spanMessage.innerText = "You clicked Third button";
-    }
+    spanMessage.innerText = "You clicked " + button.innerText + " button";
 })
